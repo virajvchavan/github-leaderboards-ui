cafc2166b2d4ef1b36ace9c16abb729f2f646510
Nilam Chavan <nilam4444@gmail.com>
2020-09-13 12:51:37 +0530

    added files

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ const fetchData = async () => {
 
 const generateHTML = (users) => {
     let tableEle = document.getElementById("leaderboard-table");
-    let htmlToAppend = `<tr>
-        <th>Username</th>
-        <th>Merged PRs</th>
-        <th>Closed PRs</th>
-        <th>Open PRs</th>
+    let htmlToAppend = `<tr class="heading-row">
+        <th class="table-heading center">Username</th>
+        <th class="table-heading center">Merged PRs</th>
+        <th class="table-heading center">Closed PRs</th>
+        <th class="table-heading center">Open PRs</th>
     </tr>`;
     users.forEach(user => {
         htmlToAppend += `<tr>
-            <td><a href="https://github.com/${user.username}" target="_blank"> ${user.username}</a></td>
-            <td>${user.merged_prs || 0}</td>
-            <td>${user.closed_prs || 0}</td>
-            <td>${user.open_prs || 0}</td>
+            <td class="table-data left"><a class="username" href="https://github.com/${user.username}" target="_blank"> ${user.username}</a></td>
+            <td class="table-data center">${user.merged_prs || 0}</td>
+            <td class="table-data center">${user.closed_prs || 0}</td>
+            <td class="table-data center">${user.open_prs || 0}</td>
         </tr>`;
     });
     tableEle.innerHTML = htmlToAppend;
