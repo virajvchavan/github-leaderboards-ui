e4f263a6cc677c614f4cc871ff1adf70e033cc33
Viraj Chavan <virajc@live.com>
2020-10-11 20:07:03 +0530

    Support for any repo

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const endpoint = "https://ohwoj3u4oi.execute-api.us-east-1.amazonaws.com/dev/prs?owner=denoland&repo=deno";
+const endpoint = "https://ohwoj3u4oi.execute-api.us-east-1.amazonaws.com/dev/prs?";
 
-const fetchData = async () => {
-    let res = await fetch(endpoint);
+const fetchData = async (owner, repo) => {
+    let res = await fetch(endpoint + `owner=${owner}&repo=${repo}`);
     let data = await res.json();
     if (data.status == "done") {
         generateHTML(data.users);
+    } else if(data.status == "processing") {
+        document.getElementById("leaderboard-table").innerHTML = "<tr><th>Loading...</th></tr>";
+        setTimeout(() => {
+            fetchData(owner, repo);
+        }, 4000);
     } else {
         let errorEle = document.getElementById("error-message");
         errorEle.className = "";
@@ -31,4 +36,16 @@ const generateHTML = (users) => {
     tableEle.innerHTML = htmlToAppend;
 }
 
-fetchData();
+// fetchData();
+document.getElementById("btn-submit").addEventListener("click", function(evt) {
+    let value = document.getElementById("repoName").value;
+    if (value.indexOf("github.com/")) {
+        value = value.replace("https://github.com/", "").replace("http://github.com/", "").replace("github.com/", "");
+    }
+    if (value.split("/").length !== 2) {
+        alert("Invalid repo url");
+        return;
+    }
+    const [owner, repo] = value.split("/");
+    fetchData(owner, repo);
+});
